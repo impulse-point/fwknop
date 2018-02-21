797b3959947098aa0efa12f72964ad1ea40e343a
Jeffrey Price <jprice@impulse.com>
2018-02-21 17:05:01 -0500

    AssemblyInfo file

---
+++ impulse/Fwknop/FwknopDotNetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Reflection;
+
+[assembly: System.Reflection.AssemblyCompanyAttribute("Fwknop")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("Fwknop")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Fwknop")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
